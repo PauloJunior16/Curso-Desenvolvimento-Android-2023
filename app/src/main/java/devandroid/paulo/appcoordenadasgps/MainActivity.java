0819f7571a59101bc2eab9ece4187cc0421c22f4
Paulo Junior <pcjsouza16@gmail.com>
2023-04-23 19:03:13 -0300

    Implementação de regra para solicitar permissão dos dados de localização

--- app/src/main/java/devandroid/paulo/appcoordenadasgps/MainActivity.java
+++ app/src/main/java/devandroid/paulo/appcoordenadasgps/MainActivity.java
@@ -1,15 +1,33 @@
 package devandroid.paulo.appcoordenadasgps;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    String[] permissoesRequiridas = {
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+    };
+
+    public static final int APP_PERMISSOES_ID = 2023;
 
     TextView txtValorLatitude, txtValorLongitude;
     double latitude, longitude;
+
+    LocationManager locationManager;
     boolean gpsAtivo = true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +36,10 @@ public class MainActivity extends AppCompatActivity {
 
         txtValorLatitude = findViewById(R.id.txtValorLatitude);
         txtValorLongitude = findViewById(R.id.txtValorLongitude);
+
+        locationManager = (LocationManager) getApplication().getSystemService(Context.LOCATION_SERVICE);
+
+        gpsAtivo = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
 
         if (gpsAtivo) {
 
@@ -36,20 +58,37 @@ public class MainActivity extends AppCompatActivity {
 
     private void obterCoordenadas(){
 
-        boolean permissaoAtiva = true;
+        boolean permissaoAtiva = solicitarPermissaoParaObterLocalizacao();
 
         if (permissaoAtiva) {
             capturarUltimaLocalizacaoValida();
-        } else {
-            solicitarPermissaoParaObterLocalizacao();
         }
-
-
     }
 
-    private void solicitarPermissaoParaObterLocalizacao() {
+    private boolean solicitarPermissaoParaObterLocalizacao() {
 
         Toast.makeText(this, "Aplicação não tem permissão de localização...", Toast.LENGTH_LONG).show();
+
+        List<String> permissoesNegadas = new ArrayList<>();
+
+        int permissaoNegada;
+
+        for (String permissao : this.permissoesRequiridas) {
+
+            permissaoNegada = ContextCompat.checkSelfPermission(MainActivity.this, permissao);
+
+            if(permissaoNegada !=PackageManager.PERMISSION_GRANTED) {
+                permissoesNegadas.add(permissao);
+            }
+        }
+
+        if(!permissoesNegadas.isEmpty()) {
+            ActivityCompat.requestPermissions(MainActivity.this, permissoesNegadas.toArray(new String[permissoesNegadas.size()]), APP_PERMISSOES_ID);
+
+            return false;
+        } else {
+            return true;
+        }
     }
 
     private void capturarUltimaLocalizacaoValida() {
